35e560e0fa2c7b73f29ac01a6f2ac1b4b9fb2b87
IyarGross <iyargross10@gmail.com>
2024-11-05 18:39:45 +0200

    Add files via upload

--- MultiTree.h
+++ MultiTree.h
@@ -18,4 +18,41 @@ public:
     bool removeRecord(const T& key);
     void print(MultiTreeNode<T>* current,int level) const;
 };
+
+// Implementation
+
+template <typename T>
+MultiTree<T>::~MultiTree() {
+    // TODO: complete
+}
+
+template <typename T>
+bool MultiTree<T>::addRecord(const T& parentKey, NodeRecord<T>* record) {
+    // TODO: complete
+    return false;
+}
+
+
+template <typename T>
+bool MultiTree<T>::removeRecord(const T& key) {
+    // TODO: complete
+    return false;
+}
+
+
+template <typename T>
+NodeRecord<T>* MultiTree<T>::searchRecord(const T& key) {
+    // TODO: complete
+    return nullptr;  // Return nullptr if not found
+}
+
+
+template <typename T>
+void MultiTree<T>::print(MultiTreeNode<T>* current,int level) const {
+    for (int i = 0; i < level; i++)
+        cout << "   ";
+    cout << current->content;
+    for (auto it = current->responses.begin(); it != current->responses.end(); it++)
+        print(*it, level + 1);
+}
 #endif  // MULTITREE_H
